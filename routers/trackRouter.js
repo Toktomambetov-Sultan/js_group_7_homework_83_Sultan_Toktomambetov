0e40448b97abe1a0a7feb8df2e10fd2f2055a908
sultan <sultan.toktomambetov.04@bk.ru>
2020-11-02 01:22:13 +0600

    Did router for tracks.

--- routers/trackRouter.js
+++ routers/trackRouter.js
@@ -5,14 +5,21 @@ const schema = require("./../Models");
 router.get("/", async (req, res) => {
   let tracks;
   try {
-    
-    tracks =req.query && req.query.author? await schema.Track.find(req.query).populate({
-      path: "album",
-      populate: "author",
-    }):
-    await schema.Track.find() 
-
-
+    tracks =
+      req.query && req.query.author
+        ? (
+            await Promise.all(
+              (
+                await schema.Album.find({
+                  author: req.query.author,
+                })
+              ).map((item) => schema.Track.find({ album: item._id }))
+            )
+          ).reduce((item, acc) => [...acc, ...item], [])
+        : await schema.Track.find(req.query).populate({
+            path: "album",
+            populate: "author",
+          });
   } catch (error) {
     res.send(error);
   }
